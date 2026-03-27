8cda1e763b1d70cc53026631ebb77d47525e4973
estjuanitaizambr1-glitch <est.juanitai.zambr1@unimilitar.edu.co>
2026-03-26 23:26:17 -0500

    Función para dibujar la curva

--- TALLER2-2.js
+++ TALLER2-2.js
@@ -10,3 +10,35 @@ let points = [
 ];
 // Punto que se está arrastrando
 let draggingPoint = null;
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Líneas de control
+  ctx.beginPath();
+  ctx.moveTo(points[0].x, points[0].y);
+  ctx.lineTo(points[1].x, points[1].y);
+  ctx.lineTo(points[2].x, points[2].y);
+  ctx.lineTo(points[3].x, points[3].y);
+  ctx.strokeStyle = "#aaa";
+  ctx.stroke();
+
+  // Curva Bézier
+  ctx.beginPath();
+  ctx.moveTo(points[0].x, points[0].y);
+  ctx.bezierCurveTo(
+    points[1].x, points[1].y,
+    points[2].x, points[2].y,
+    points[3].x, points[3].y
+  );
+  ctx.strokeStyle = "blue";
+  ctx.lineWidth = 2;
+  ctx.stroke();
+
+  // Dibujar puntos
+  points.forEach(p => {
+    ctx.beginPath();
+    ctx.arc(p.x, p.y, 6, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+  });
+}
