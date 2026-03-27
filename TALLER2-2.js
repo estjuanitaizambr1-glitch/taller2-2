08f50012f855fbed0c8e31c20205f8586d41f63f
estjuanitaizambr1-glitch <est.juanitai.zambr1@unimilitar.edu.co>
2026-03-26 23:10:25 -0500

    Puntos de control de la curva Bézier

--- TALLER2-2.js
+++ TALLER2-2.js
@@ -1,3 +1,10 @@
 // Obtener canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+// Puntos iniciales
+let points = [
+  { x: 50, y: 300 },
+  { x: 150, y: 50 },
+  { x: 350, y: 50 },
+  { x: 450, y: 300 }
+];
